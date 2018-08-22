fd660ad60830d42aaef4afd67a32e481b8e6dbeb
Won Maung Thein <wonmaungthein@gmail.com>
2018-08-22 09:13:49 +0100

    created input form and tested

---
+++ cypress/integration/input-form.spec.js
@@ -0,0 +1,15 @@
+describe("Input form", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("focuses input on load", () => {
+    cy.focused().should("have.class", "new-todo");
+  });
+  it("accepts input", () => {
+    const typedText = "Buy Milk";
+
+    cy.get(".new-todo")
+      .type(typedText)
+      .should("have.value", typedText);
+  });
+});
